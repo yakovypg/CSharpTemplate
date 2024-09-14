5b0ccc194e037f52898c96ed5086fe2c96351f24
yakovypg <yasha-pmp@mail.ru>
2024-09-14 12:00:00 +0300

    Add console project

---
+++ CSharpTemplate.ConsoleProject/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpTemplate.ConsoleProject;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Arguments not specified");
+            return;
+        }
+
+        foreach (string arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+    }
+}
